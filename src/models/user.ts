b065c13828db4f98dc91fcbf17101a8843107ba0
hadi-bakhshi <hadibakhshi277@gmail.com>
2022-09-29 18:52:00 +0330

    models added

---
+++ src/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', UserSchema);
